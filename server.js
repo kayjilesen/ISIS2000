9d5d55d1359cee055c7427e9a9d5e3c5e9b6074e
Kay Jilesen <k.jilesen@student.fontys.nl>
2018-12-06 10:54:51 -0700

    Heroku Update

--- server.js
+++ server.js
@@ -9,9 +9,13 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     next();
-})
+});
 
 var server = app.listen(process.env.PORT || 8080, function(){
     var port = server.address().port;
     console.log("App now running on port ", port);
-})
+});
+
+app.get("/", function(req, res){
+    res.sendfile("public/index.html", { root: __dirname + "/public"});
+});
